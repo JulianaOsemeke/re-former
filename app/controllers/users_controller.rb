04e97815cdae16c6ff001829174b72a4b17d6cea
samroodAli <devsamrood@gmail.com>
2021-04-28 18:04:15 +0530

    Fix forms using form_tag method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 def create
   @user = User.new(user_params)
-
   if @user.save
     redirect_to new_user_url
   else
@@ -10,6 +9,7 @@ def create
 end
 
   def new
+    @user = User.new
   end
 
   private
