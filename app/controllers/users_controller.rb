52b15d3ebffff9ee84c2e77c3d6cbf61c556a52e
samroodAli <devsamrood@gmail.com>
2021-04-28 15:50:15 +0530

    Add create user post method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render "new"
+    end
   end
 
   def new
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:password,:email)
   end
 end
