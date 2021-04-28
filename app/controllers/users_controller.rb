1ab83d12899833ce10bd84428c2c6df72708b176
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-04-28 16:56:12 +0100

    Correct linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
-def create
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to new_user_url
-  else
-    render :new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_url
+    else
+      render :new
+    end
   end
-end
 
   def new
-    @user = User.new(name: "Jules" , email: "juliana.osemeke@gmail.com")
+    @user = User.new(name: 'Jules', email: 'juliana.osemeke@gmail.com')
   end
 
   def edit
     @user = User.find(params[:id])
   end
-
 
   def index
     @users = User.all
@@ -27,14 +26,12 @@ end
 
   def update
     @user = User.find_by(params[:id])
-    if @user.update(user_params)
-      render :show
-    end
+    render :show if @user.update(user_params)
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:name,:email,:password)
+    params.require(:user).permit(:name, :email, :password)
   end
-  
 end
